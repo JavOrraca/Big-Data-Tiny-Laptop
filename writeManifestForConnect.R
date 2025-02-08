9c0f6bc658582f29ee77b317c3c8d865664251bb
Javier Orraca <orraca.javier@gmail.com>
2025-02-07 18:31:55 -0800

    Revising main app name to `index.qmd` for deployment to GitHub Pages

--- writeManifestForConnect.R
+++ writeManifestForConnect.R
@@ -3,5 +3,5 @@ library(rsconnect)
 rsconnect::writeManifest(
   appDir = ".", 
   appMode = "quarto-static", 
-  appFiles = c("big-data-tiny-laptop.qmd", "big-data-tiny-laptop.html", 
+  appFiles = c("index.qmd", "index.html", "Big_Data_Tiny_Laptop.png", 
   "_brand.yml", "styling.scss", list.files("images/", recursive = T, full.names = T)))
